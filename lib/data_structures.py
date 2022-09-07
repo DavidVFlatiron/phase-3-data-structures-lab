01837f0375805a7f72b573966cdaa9dc05640c40
David Verghese <davidverghese@davidvehesesmbp.myfiosgateway.com>
2022-09-07 18:32:47 -0400

    finished

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,19 +17,43 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    solution = list()
+    for spicy_food in spicy_foods: 
+        solution.append(spicy_food['name'])
+    return solution
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    solution = list()
+    for spicy_food in spicy_foods: 
+        if spicy_food['heat_level'] > 5:
+            solution.append(spicy_food)
+    return solution
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for spicy_food in spicy_foods:
+        heat_level = spicy_food['heat_level']
+        peppers = '🌶' * heat_level
+        name = spicy_food['name']
+        cuisine = spicy_food['cuisine']
+        message =  f'{name} ({cuisine}) | Heat Level: {peppers}'
+        print(message)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for spicy_food in spicy_foods:
+        spicy_food_cuisine = spicy_food['cuisine']
+        spicy_food_name = spicy_food['name']
+        spicy_food_heat_level = spicy_food['heat_level']
+        if spicy_food_cuisine == cuisine: 
+            solution = {"name": spicy_food_name, "cuisine": spicy_food_cuisine,"heat_level": spicy_food_heat_level}
+            return solution
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spicy_enough = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spicy_enough)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    solution = 0
+    length = len(spicy_foods)
+    for spicy_food in spicy_foods:
+        solution += spicy_food['heat_level']
+    return solution/length
